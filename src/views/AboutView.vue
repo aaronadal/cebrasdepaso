2e86e1dc4cafda12970bc7502600beff8baf0df2
Aarón Nadal <aaronadal.dev@gmail.com>
2022-08-04 17:34:19 +0200

    Some content for about.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section class="container">
+      <h1>¿Qué es CEBRAS DE PASO?</h1>
+      <p>CEBRAS DE PASO es lo que acontece cuando se juntan dos mentes quizá no del todo sanas y
+        una frase cuyo poder es casi ilimitado.</p>
+      <p>Las dos mentes son las nuestras (más adelante te contamos quiénes somos en detalle), y
+        la frase es "¡Puede ser divertido!".</p>
+    </section>
+    <section class="container">
+      <h2>¿Por qué CEBRAS y por qué DE PASO?</h2>
+    </section>
+    <section class="container">
+      <h2>¿Por qué un pódcast?</h2>
+      <p>Es curioso. Solemos hablar mucho, incluso aunque no tengamos dos micrófonos delante.
+        Cuando lo hacemos, surgen temáticas y reflexiones de lo más interesante. El problema es
+        que, unas veces porque no tenemos tiempo y otras porque divagamos y nos pasamos de CEBRAS,
+        casi nunca llegamos a profundizar lo suficiente.</p>
+      <p>Grabar un pódcast es la excusa perfecta para charlar sobre una temática concreta durante
+        un tiempo suficiente para alcanzar cierto grado de profundidad. </p>
+      <p>Grabar un pódcast es la excusa perfecta para charlar sobre una temática concreta durante
+        un tiempo suficiente y alcanzar cierto grado de profundidad.</p>
+      <p>Además, tenemos la certeza de que sois mucha más gente con intereses similares a los nuestros
+        ahí fuera en la infinidad del Internet y, oye, nos parece una buena idea poder conoceros.</p>
+    </section>
+    <section class="container">
+      <h2>¿Quiénes conforman la manada?</h2>
+    </section>
   </div>
 </template>
