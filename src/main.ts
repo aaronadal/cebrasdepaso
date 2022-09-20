ec997d0fd995814d72f678374db8f91b9e8158eb
Aarón Nadal <aaronadal.dev@gmail.com>
2022-09-20 08:49:10 +0200

    Vue on window load

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App).use(router)
+
+window.addEventListener("load", function() {
+    app.mount('#app')
+});
