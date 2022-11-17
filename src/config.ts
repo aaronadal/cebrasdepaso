e08be15bb6b31ae089194c93827cf0d0ddb2b434
Aarón Nadal <aaronadal.dev@gmail.com>
2022-11-17 17:20:15 +0100

    Parse title and description.

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,6 @@ import {Episode} from "@/media";
 
 export const PUBLISHED = false;
 
-// export const PODCAST_RSS_URL = 'https://anchor.fm/s/69f84e7c/podcast/rss' // No tiene nombre.
-// export const PODCAST_RSS_URL = 'https://anchor.fm/s/a1edc924/podcast/rss' // Filosofía y humor.
-// export const PODCAST_RSS_URL = 'https://anchor.fm/s/10468614/podcast/rss' // Filosofía de bolsillo.
-
 export const PODCAST_SPOTIFY_URL = '';
 export const PODCAST_GOOGLE_PODCASTS_URL = '';
 export const PODCAST_APPLE_PODCASTS_URL = '';
@@ -13,7 +9,8 @@ export const PODCAST_AMAZON_MUSIC_URL = '';
 export const PODCAST_IVOOX_URL = '';
 export const PODCAST_POCKETCASTS_URL = '';
 export const PODCAST_OVERCAST_URL = '';
-export const PODCAST_RSS_URL = 'https://anchor.fm/s/a4e7fe88/podcast/rss' // La Ruina.
+export const PODCAST_RSS_URL = 'https://anchor.fm/s/c0099e38/podcast/rss' // 'https://anchor.fm/s/c0099e38/podcast/rss'
+                                                                          // 'https://anchor.fm/s/a4e7fe88/podcast/rss'
 
 export const PODCAST_INTRODUCTION_EPISODE = {
     title: 'Esto es... CEBRAS DE PASO',
--- src/rss.ts
+++ src/rss.ts
@@ -1,4 +1,4 @@
-import { Track, Episode, Podcast } from "@/media";
+import {Episode, Podcast, EpisodeType} from "@/media";
 
 function parseDate (date: string): Date {
   return new Date(date)
@@ -19,21 +19,32 @@ function parseEpisodes (data: Document): Episode[] {
   for (let i = items.length - 1; i >= 0; i--) {
     const item = items[i]
 
-    const episodeType = (item.getElementsByTagNameNS(itunesNS, 'episodeType')?.[0]?.textContent || 'full') as 'full'|'bonus'|'trailer'
+    const episodeType = (item.getElementsByTagNameNS(itunesNS, 'episodeType')?.[0]?.textContent || 'full') as EpisodeType
     const relatedUrl = item.querySelector('link')?.textContent || ''
     const media = item.querySelector('enclosure')
     const artworkSrc = item.getElementsByTagNameNS(itunesNS, 'image')?.[0]?.getAttribute('href') || ''
     const artworkExt = artworkSrc.split('.').slice(-1)[0]?.toLowerCase() || ''
 
     counts[episodeType]++
+    let number = counts[episodeType];
+    let title = item.querySelector('title')?.textContent || '';
+    if(/\[#\d]\s/.test(title.substring(0, 6))) {
+      number = parseInt(title.substring(2, 5));
+      title = title.substring(7);
+    }
+
+    let summary = item.querySelector('description')?.textContent || '';
+    if(title.includes('<p>---</p>')) {
+      summary = title.split('<p>---</p>')[0];
+    }
 
     episodes[i] = {
       guid: item.querySelector('guid')?.textContent || '',
       date: parseDate(item.querySelector('pubDate')?.textContent || ''),
-      title: item.querySelector('title')?.textContent || '',
-      summary: item.querySelector('description')?.textContent || '',
+      title,
+      summary,
       episodeType,
-      number: counts[episodeType],
+      number,
       season: parseInt(item.getElementsByTagNameNS(itunesNS, 'season')?.[0]?.textContent || '0'),
       numberInSeason: parseInt(item.getElementsByTagNameNS(itunesNS, 'episode')?.[0]?.textContent || '0'),
       duration: parseInt(item.getElementsByTagNameNS(itunesNS, 'duration')?.[0]?.textContent || '0'),
