6c5078acb3ff4cb0735e966b9e60e1dc4e21e129
Aarón Nadal <aaronadal.dev@gmail.com>
2022-11-29 18:05:58 +0100

    Add Google Podcasts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,9 @@
 export const PUBLISHED = true;
 
 export const PODCAST_SPOTIFY_URL = 'https://open.spotify.com/show/2PzTJfAK2kIXXrVaBAqhTp';
-export const PODCAST_GOOGLE_PODCASTS_URL = null;
+export const PODCAST_GOOGLE_PODCASTS_URL = 'https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy9jMDA5OWUzOC9wb2RjYXN0L3Jzcw';
 export const PODCAST_APPLE_PODCASTS_URL = 'https://podcasts.apple.com/us/podcast/cebras-de-paso/id1655554658';
 export const PODCAST_AMAZON_MUSIC_URL = 'https://music.amazon.es/podcasts/730b541d-44a7-415d-b91a-f3db5d12929a/cebras-de-paso';
-export const PODCAST_IVOOX_URL = null;
 export const PODCAST_POCKETCASTS_URL = 'https://pca.st/106f084z';
 export const PODCAST_OVERCAST_URL = 'https://overcast.fm/itunes1655554658';
 export const PODCAST_RSS_URL = 'https://anchor.fm/s/c0099e38/podcast/rss'
