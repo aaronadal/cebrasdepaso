1adf7460b42e62f9b8efc8faa75ab2c867c82f78
Aarón Nadal <aaronadal.dev@gmail.com>
2024-07-30 12:18:54 +0200

    Update routes rendering config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-    devtools: {
-        enabled: false
-    },
-    imports: {
-        autoImport: false
-    },
-    app: {
-        pageTransition: {
-            name: 'fade',
-            mode: 'out-in'
-        }
-    },
-    components: [
-        {
-            path: '~/components',
-            pathPrefix: false,
-        },
-    ],
-    routeRules: {
-        '/': { prerender: true },
-        '/contacto': { prerender: true },
-        '/aviso-legal': { prerender: true },
-        '/generador-de-caratulas': { ssr: false },
-        '/generador-de-logotipos': { ssr: false },
-        '/generador-de-degradados': { ssr: false },
-    }
+  devtools: {
+      enabled: false
+  },
+
+  imports: {
+      autoImport: false
+  },
+
+  app: {
+      pageTransition: {
+          name: 'fade',
+          mode: 'out-in'
+      }
+  },
+
+  components: [
+      {
+          path: '~/components',
+          pathPrefix: false,
+      },
+  ],
+
+  routeRules: {
+      '/': { prerender: true },
+      '/aviso-legal': { prerender: true },
+      '/contacto': { prerender: true },
+      '/generador-de-caratulas': { ssr: false },
+      '/generador-de-logotipos': { ssr: false },
+      '/generador-de-degradados': { ssr: false },
+      '/podcast': { swr: true },
+      '/ultimo': { ssr: false },
+  },
+
+  compatibilityDate: '2024-07-30'
 })
